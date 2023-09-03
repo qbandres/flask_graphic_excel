7e97d6bedf1b578417f78e213dbc241195ba411a
qbandres <qbandres@gmail.com>
2023-09-03 03:09:01 -0500

    Se modifico para la web

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# Se configuro para deploy render rev2
-
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -17,32 +17,33 @@ def index():
             df = pd.read_excel(file)
             df_upload = df
             html_table = df.to_html(classes='table table-bordered table-striped', index=False)
-    
-    return render_template('index.html', html_table=html_table)
+
+    return render_template('index.html', html_table=html_table, img_base64=None)
 
 @app.route('/graphics')
 def graphics():
+    global df_upload
 
-    # pivot_table = df_upload.pivot(index='item', columns='Especialidad', values='Cant')
-    group_table = df_upload[['Especialidad','Cant']].groupby(['Especialidad']).sum()
+    group_table = df_upload[['Especialidad', 'Cant']].groupby(['Especialidad']).sum()
     tgroup = group_table.transpose()
-    ax = tgroup.plot(kind='bar', stacked = False)
-    # Agregar etiquetas de cantidad a las barras importante
+    ax = tgroup.plot(kind='bar', stacked=False)
+
     for p in ax.patches:
         ax.annotate(str(p.get_height()), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='bottom')
 
     plt.title('Distribución de Personal')
-    # plt.xlabel('Cargo')
-    # plt.ylabel('Cant.')
 
-    plt.show()
+    # Guarda el gráfico como una imagen en memoria
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    # Convierte la imagen en una cadena base64
+    img_base64 = base64.b64encode(img.read()).decode('utf-8')
+
     plt.close()
 
-    return redirect(url_for('index'))
-
-
+    return render_template('index.html', img_base64=img_base64, html_table=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
